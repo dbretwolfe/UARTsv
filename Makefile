36980dbbce7c7badc84bf6a78a52810d8e1492ac
unknown <dwolf@CLEAVER.ds.cecs.pdx.edu>
2015-12-05 14:22:13 -0800

    Devin
    Removed test package

---
+++ Makefile
@@ -0,0 +1,56 @@
+#Sameer Ghewari, Portland State University, Feb 2015
+#This makefile is for TBX BFM Example - Simple booth
+
+#Specify the mode- could be either puresim or veloce
+#Always make sure that everything works fine in puresim before changing to veloce
+
+MODE ?= puresim
+
+#make all does everything
+all: work build run
+
+#Create respective work libs and map them 
+work:
+	vlib work.$(MODE)
+	vmap work work.$(MODE)
+	
+#Compile/synthesize the environment
+build:
+	vlog TopHVL.sv			#Compile the testbench 
+ifeq ($(MODE),puresim)		#If mode is puresim, compile everything else
+	vlog UARTIf_HDL.sv				#Compile the interface
+	vlog UART_Top.sv					#Compile the UART DUT
+	vlog TopHDL.sv				#Compule the HDL top 
+	vlog Timing_Gen.sv
+	vlog TX_FSM.sv
+	vlog Receiver.sv
+	vlog BIST.sv
+	vlog FIFO.sv
+	velhvl -sim $(MODE)
+else						#else, synthesize!
+	velanalyze -extract_hvl_info +define+QUESTA TopHVL.sv	#Analyze the HVL for external task calls in BFM 
+	velanalyze UARTIf_HDL.sv			#Analyze the interface for synthesis
+	velanalyze TopHDL.sv		#Analyze the HDL top for synthesis 
+	velanalyze UART_Top.sv			#Analyze the UART DUT for synthesis
+	velanalyze Timing_Gen.sv
+	velanalyze TX_FSM.sv
+	velanalyze Receiver.sv
+	velanalyze BIST.sv
+	velanalyze FIFO.sv
+	velcomp -top TopHDL  	#Synthesize!
+	velhvl -sim $(MODE) 
+endif
+
+run:
+	vsim -c -do "run -all" TopHVL TopHDL	#Run all 
+	cp transcript transcript.$(MODE)		#Record transcript 
+
+norun:	#No run lets you control stepping etc. 
+	vsim -c +tbxrun+norun TopHVL TopHDL -cpppath $(CPP_PATH)
+	cp transcript transcript.$(MODE)
+
+clean:
+	rm -rf tbxbindings.h modelsim.ini transcript.veloce transcript.puresim work work.puresim work.veloce transcript *~ vsim.wlf *.log dgs.dbg dmslogdir veloce.med veloce.wave veloce.map velrunopts.ini edsenv 
+	
+
+
--- TestPkg.sv
+++ TestPkg.sv
@@ -1,71 +1,6 @@
-interface UART_IFace;
+package TestPkg;
 
-	parameter SYSCLK_RATE = 100000000;
-	parameter BAUD_RATE = 9600;
-	parameter DATA_BITS = 8;
-	parameter PARITY_BIT = 1;
-	parameter STOP_BITS = 2;
-	parameter FIFO_DEPTH = 8;
-	
-	localparam TX_BITS = (1 + DATA_BITS + 1 + STOP_BITS);
-
-	//pragma attribute UART_IFace partition_interface_xif 
-	logic 					SysClk;
-	logic					Clk;
-	logic 					Rst;
-	logic 					Rx;
-	logic 					CTS;
-	logic 					Tx;
-	logic 					RTS;
-	logic 					BIST_Start;
-	logic [DATA_BITS-1:0] 	Tx_Data;
-	logic 					Transmit_Start;
-	logic					Tx_Busy;
-	logic [DATA_BITS-1:0] 	Data_Out;
-	logic 					Data_Rdy;
-	logic [2:0] 			Rx_Error;
-	logic 					BIST_Busy;
-	logic 					BIST_Error;
-	logic 					Read_Done;		// Input to FIFO to cycle new data onto output
-	logic					FIFO_Empty;		// Output from FIFO - no data
-	logic					FIFO_Full; 		// Output from FIFO - 
-	logic					FIFO_Overflow;		// Output from FIFO
-	
-	//**************************************
-	//
-	//	Internal Tasks
-	//
-	//**************************************
-	
-	task automatic WriteData(logic [DATA_BITS-1:0] WriteBuf); //pragma tbx xtf
-		while (Tx_Busy)	// Wait until the current transmission is finished, if any
-			@(posedge SysClk);
-		Tx_Data = WriteBuf;	// Set the transmit data reg
-		@(negedge SysClk);	// On the next negative clock edge,
-		Transmit_Start = '1;	// assert transmit start.
-		@(negedge Tx);
-		Transmit_Start = '0;	// Hold transmit start until the start bit is set on Tx.  The 
-					// transmission should now be started.
-	endtask
-
-	task automatic ReadData(ref logic [DATA_BITS-1:0] ReadBuf); //pragma tbx xtf
-		while (FIFO_Empty)// Make sure the fifo is not empty
-			@(posedge SysClk);
-		@(posedge SysClk);
-		Read_Done = '1;		// Strobe the Read_Done input to tell the FIFO to cycle
-		@(posedge SysClk);
-		Read_Done = '0;		// in new data.
-		ReadBuf = Data_Out; 	// Copy the data from the FIFO output
-	endtask
-	
-	task automatic Start_BIST(); //pragma tbx xtf
-		BIST_Start = '1;
-		while(!BIST_Busy)
-			@(posedge SysClk);
-		BIST_Start = '0;
-	endtask
-	
-		//***************************************************
+	//***************************************************
 	//	Testbench tasks
 	//
 	//  Return the test results to the HVL module
@@ -223,11 +158,12 @@ interface UART_IFace;
 				Result = 0;
 	endtask
 
+
+	
 	// Simple task to perform a system wide reset
 	task automatic DoReset(); //pragma tbx xtf
 		Rst = '1;
 		@(posedge SysClk);
 		Rst = '0;
 	endtask
-			
-endinterface
+endpackage
