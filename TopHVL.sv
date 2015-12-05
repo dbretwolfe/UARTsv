932bf38d20659680107e3acf4989a84d8d657d47
unknown <dwolf@CLEAVER.ds.cecs.pdx.edu>
2015-12-05 15:55:20 -0800

    Devin

--- TopHVL.sv
+++ TopHVL.sv
@@ -20,71 +20,63 @@ initial begin
 	
 	// The first two directed tasks check sending all zeroes and all ones.
 	TopHDL.TestIf.CheckTransmit(8'h00, result);
-	CheckResult(.result, .testsFailed, .numTestsFailed);
+	CheckResult(.result(result), .testsFailed(testsFailed), .numTestsFailed(numTestsFailed));
 	`ifdef DEBUG
 		if (result)
 			$display("Transmit check failed!");
-		end
 	`endif
 	
 	TopHDL.TestIf.CheckTransmit(8'hFF, result);
-	CheckResult(.result, .testsFailed, .numTestsFailed);
+	CheckResult(.result(result), .testsFailed(testsFailed), .numTestsFailed(numTestsFailed));
 	`ifdef DEBUG
 		if (result)
 			$display("Transmit check failed!");
-		end
 	`endif
 	
 	// The next task fills the FIFO completely, reads the FIFO data, and compares the received
 	// data to the sent data.
 	TopHDL.TestIf.Fill_FIFO(result);
-	CheckResult(.result, .testsFailed, .numTestsFailed);
+	CheckResult(.result(result), .testsFailed(testsFailed), .numTestsFailed(numTestsFailed));
 	`ifdef DEBUG
 		if (result)
 			$display("Fill FIFO task failed!");
-		end
 	`endif
 	
 	//This task exercises the BIST system.  
 	TopHDL.TestIf.BIST_Check(8'h00, result);
-	CheckResult(.result, .testsFailed, .numTestsFailed);
+	CheckResult(.result(result), .testsFailed(testsFailed), .numTestsFailed(numTestsFailed));
 	`ifdef DEBUG
 		if (result)
 			$display("BIST check failed!");
-		end
 	`endif
 	
 	TopHDL.TestIf.BIST_Check(8'hFF, result);
-	CheckResult(.result, .testsFailed, .numTestsFailed);
+	CheckResult(.result(result), .testsFailed(testsFailed), .numTestsFailed(numTestsFailed));
 	`ifdef DEBUG
 		if (result)
 			$display("BIST check failed!");
-		end
 	`endif
 	
 	// The following 3 tasks stimulate all of the possible Rx error signals.
 	TopHDL.TestIf.SendData_ParityError(result);
-	CheckResult(.result, .testsFailed, .numTestsFailed);
+	CheckResult(.result(result), .testsFailed(testsFailed), .numTestsFailed(numTestsFailed));
 	`ifdef DEBUG
 		if (result)
 			$display("Failed to produce Rx parity error!");
-		end
 	`endif
 	
 	TopHDL.TestIf.SendData_FrameError(result);
-	CheckResult(.result, .testsFailed, .numTestsFailed);
+	CheckResult(.result(result), .testsFailed(testsFailed), .numTestsFailed(numTestsFailed));
 	`ifdef DEBUG
 		if (result)
 			$display("Failed to produce Rx frame error!");
-		end
 	`endif
 	
 	TopHDL.TestIf.SendData_BreakError(result);
-	CheckResult(.result, .testsFailed, .numTestsFailed);
+	CheckResult(.result(result), .testsFailed(testsFailed), .numTestsFailed(numTestsFailed));
 	`ifdef DEBUG
 		if (result)
 			$display("Failed to produce Rx break error!");
-		end
 	`endif
 	
 	if (!testsFailed)
