888321b3e70217b5bf0b980b316982f9b4544eca
unknown <dwolf@CLEAVER.ds.cecs.pdx.edu>
2015-12-05 16:56:30 -0800

    DW

--- TopHVL.sv
+++ TopHVL.sv
@@ -66,7 +66,7 @@ initial begin
 			$display("Failed to produce Rx parity error! err = %h State = %s", Err, TopHDL.TestUART.Receiver.State);
 			
 	`endif
-	TopHDL.TesIf.wait8();
+	TopHDL.TestIf.wait8();
 	
 	TopHDL.TestIf.SendData_FrameError(result, Err);
 	CheckResult(.result(result), .testsFailed(testsFailed), .numTestsFailed(numTestsFailed));
@@ -74,7 +74,7 @@ initial begin
 		if (result)
 			$display("Failed to produce Rx frame error! err = %h State = %s", Err, TopHDL.TestUART.Receiver.State);
 	`endif
-	TopHDL.TesIf.wait8();
+	TopHDL.TestIf.wait8();
 	
 	TopHDL.TestIf.SendData_BreakError(result, Err);
 	CheckResult(.result(result), .testsFailed(testsFailed), .numTestsFailed(numTestsFailed));
@@ -82,7 +82,7 @@ initial begin
 		if (result)
 			$display("Failed to produce Rx break error! err = %h State = %s", Err, TopHDL.TestUART.Receiver.State);
 	`endif
-	TopHDL.TesIf.wait8();
+	TopHDL.TestIf.wait8();
 	
 	if (!testsFailed)
 		$display("All tests have passed!");
