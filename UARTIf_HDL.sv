aebb003616d97243174858cb5c1c5db5e15ca229
Devin Wolfe <devin@wolfepac.net>
2016-03-05 17:43:09 -0800

    dw

--- UARTIf_HDL.sv
+++ UARTIf_HDL.sv
@@ -12,15 +12,15 @@ interface UART_IFace;
 
 //////////////////    Assertion Block     ////////////////////
 //assert range of SysCLK_Rate
-assert ((SYSCLK_RATE > 3) && (SYSCLK_RATE<1000000000000)) else $error("SYSCLK_RATE is not in bounds");  
+assert property((SYSCLK_RATE > 3) && (SYSCLK_RATE<1000000000000)) else $error("SYSCLK_RATE is not in bounds");  
 //assert range of Baud_Rate  
-assert ((BAUD_RATE > 0) && (BAUD_RATE<7000000)) else $error("BAUD_RATE is not in bounds");  
+assert property((BAUD_RATE > 0) && (BAUD_RATE<7000000)) else $error("BAUD_RATE is not in bounds");  
 //assert range of Data Bits  
-assert ((DATA_BITS > 0) && (DATA_BITS<9)) else $error("DATA_BITS is not in bounds");  
+assert property((DATA_BITS > 0) && (DATA_BITS<9)) else $error("DATA_BITS is not in bounds");  
 //assert range of Stop_bits  
-assert ((STOP_BITS > 0) && (STOP_BITS<3)) else $error("STOP_BITS is not in bounds");  
+assert property((STOP_BITS > 0) && (STOP_BITS<3)) else $error("STOP_BITS is not in bounds");  
 //assert range of FIFO_Depth  
-assert ((FIFO_DEPTH > 0) && (FIFO_DEPTH<17)) else $error("FIFO_DEPTH is not in bounds");  
+assert property((FIFO_DEPTH > 0) && (FIFO_DEPTH<17)) else $error("FIFO_DEPTH is not in bounds");  
 //Assert critical relationships Baud rate, sysCLK_Rate, clock delay(below)  
 
 	//pragma attribute UART_IFace partition_interface_xif 
