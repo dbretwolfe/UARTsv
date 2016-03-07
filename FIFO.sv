2d7b9ff4b46f1f466aaaa6620bd3eb3412bb086e
dbretwolfe <dwolf@pdx.edu>
2016-03-06 16:27:45 -0800

    dw

--- FIFO.sv
+++ FIFO.sv
@@ -71,7 +71,7 @@ module FIFO # (parameter DATA_BITS = 8,	parameter FIFO_WIDTH = 4)
                         // These outputs are not changes in this part of the block
                         readPointer <= readPointer;
                         Data_Out <= Data_Out;
-						$display("Numentries  in push = %d", numEntries);
+						$display("Numentries  in push = %d", readptr = %d", numEntries, writePointer);
                     end // Write to FIFO
                     
                     else if (Pop_Data && !Data_Rdy) begin                                   // Data needs to be popped out of the FIFO
@@ -97,7 +97,7 @@ module FIFO # (parameter DATA_BITS = 8,	parameter FIFO_WIDTH = 4)
                         end
                         // The write pointer never changes in this part of the block
                         writePointer <= writePointer;
-						$display("Numentries in pop= %d, data out = %d", numEntries, Data_Out);
+						$display("Numentries in pop= %d, readptr = %d", numEntries, readPointer);
                     end // Read from FIFO
                 end // Not BIST Mode
                 else begin                                                  // Bist mode is active, don't do anything
