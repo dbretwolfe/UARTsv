ed240ea09206c3ae909278f975bfd6ea0118da92
dbretwolfe <dwolf@pdx.edu>
2016-03-06 16:11:08 -0800

    dw

--- Timing_Gen.sv
+++ Timing_Gen.sv
@@ -12,8 +12,8 @@ module Timing_Gen #(parameter SYSCLK_RATE = 100000000,
 	//	assert property(SYSCLK_RATE>0) else $error("BAUD_RATE must be more than 0");//-JF
 //	end
 	
-	localparam CLOCK_DIV = SYSCLK_RATE / BAUD_RATE; // Baud rate is transitions per second.  We get one transition per rising
-								// clock edge, meaning we need two clock transitions per baud.
+	localparam CLOCK_DIV = SYSCLK_RATE / (2*BAUD_RATE); // Baud rate is transitions per second.  We get one transition per rising
+														// clock edge, meaning we need two clock transitions per baud.
 	integer ClockCounter;
 
 	always @ (posedge SysClk or posedge Rst) begin
