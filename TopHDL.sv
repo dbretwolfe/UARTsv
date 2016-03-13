7a5d5cc5cd10c35bba903085eab223b071a1276b
jjtheman <fernowj@gmail.com>
2016-03-13 16:53:37 -0700

    Update TopHDL.sv
    I got the assertions in TopHDL.sv working(compiling) :) Jonathan

--- TopHDL.sv
+++ TopHDL.sv
@@ -22,23 +22,6 @@ module TopHDL;
 	
 	UARTsv TestUART(TestIf);
 
-//clocking initial assertions	
-	// tbx clkgen
-//	initial
-			//////////////////    Assertion Block     ////////////////////
-		//assert range of SysCLK_Rate
-		//assert ((SYSCLK_RATE > 3) && (SYSCLK_RATE<1000000000000)) else $error("SYSCLK_RATE is not in bounds");  
-		//assert range of Baud_Rate  
-		//assert ((BAUD_RATE > 0) && (BAUD_RATE<7000000)) else $error("BAUD_RATE is not in bounds");  
-		//assert range of Data Bits  
-		//assert ((DATA_BITS > 0) && (DATA_BITS<9)) else $error("DATA_BITS is not in bounds");  
-		//assert range of Stop_bits  
-		//assert ((STOP_BITS > 0) && (STOP_BITS<3)) else $error("STOP_BITS is not in bounds");  
-		//assert range of FIFO_Depth  
-		//assert ((FIFO_DEPTH > 0) && (FIFO_DEPTH<17)) else $error("FIFO_DEPTH is not in bounds");  
-		//Assert critical relationships Baud rate, sysCLK_Rate, clock delay(below) 	
-//	end
-	
 	// tbx clkgen
 	initial
 		begin
@@ -47,5 +30,22 @@ module TopHDL;
 		  #10 clk = ~clk;
 		end
 	end	
+	//////////////////    Assertion Block     ////////////////////
+//tbx clkgen
+initial
+	begin
+	//Adding agreeded upon bounds(negative check, zeros, and upper tested limits of what has been tested)
+	//assert range of SysCLK_Rate
+	assert property (@(posedge clk) ((SYSCLK_RATE > 3) && (SYSCLK_RATE<100000000))) else $error("SYSCLK_RATE is not in bounds");  
+	//assert range of Baud_Rate  
+	assert property (@(posedge clk) ((BAUD_RATE > 0) && (BAUD_RATE<7000000))) else $error("BAUD_RATE is not in bounds");  
+	//assert range of Data Bits  
+	assert property (@(posedge clk) ((DATA_BITS > 0) && (DATA_BITS<9))) else $error("DATA_BITS is not in bounds");  
+	//assert range of Stop_bits  
+	assert property (@(posedge clk) ((STOP_BITS > 0) && (STOP_BITS<3))) else $error("STOP_BITS is not in bounds");  
+	//assert range of FIFO_Depth  
+	assert property (@(posedge clk) ((FIFO_WIDTH > 0) && (FIFO_WIDTH<17))) else $error("FIFO_DEPTH is not in bounds");  
+	//Assert critical relationships Baud rate, sysCLK_Rate, clock delay(below)  
+end
 
 endmodule
