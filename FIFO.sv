ccb9edfbc1f2f1d9653718bdc573b63c1cbc67a3
rstasney <rstasney@gmail.com>
2016-03-18 14:45:59 -0700

    Update FIFO.sv

--- FIFO.sv
+++ FIFO.sv
@@ -75,11 +75,12 @@ module FIFO # (parameter DATA_BITS = 8,	parameter FIFO_WIDTH = 4)
                             									begin          // If the write pointer has caught up to the read pointer,
                                 									FIFO_Overflow = 1'b1;                         // then set the overflow flag
                             									end
+                            								numEntries = numEntries + 1; 
                         							end
                         						else 
                         							begin                                             // Otherwise nothing changes
                             								writePointer = writePointer;
-                            							//	numEntries = numEntries;
+                            								numEntries = numEntries;
                             								FIFO_Empty = FIFO_Empty;
                             								FIFO_Full = FIFO_Full;
                             								FIFO_Overflow = FIFO_Overflow;
